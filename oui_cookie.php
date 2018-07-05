8b0134b831a71a1b0dbab55df6a335cad9864255
Nicolas Morand <contact@nicolasmorand.com>
2018-07-05 15:55:59 +0200

    License related updates

--- oui_cookie.php
+++ oui_cookie.php
@@ -1,28 +1,25 @@
 <?php
 
 /*
- * oui_cookie - Set, check, read, reset or delete cookies
- *              manually or through HTTP variables.
+ * This file is part of oui_cookie,
+ * a plugin allowing easy cookies management in Textpattern CMS.
  *
  * https://github.com/NicolasGraph/oui_cookie
  *
- * Copyright (C) 2016 Nicolas Morand
+ * Copyright (C) 2018 Nicolas Morand
  *
- * This file is part of oui_cookie.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
-
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA..
  */
 
 namespace Oui {
